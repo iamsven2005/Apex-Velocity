042cbe551d68173d949223aa5520ddee5d82f132
TAN SVEN <108319539+iamsven2005@users.noreply.github.com>
2026-06-04 11:28:17 +0800

    oops

---
+++ src/components/ui/resizable.jsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { GripVertical } from "lucide-react"
+import * as ResizablePrimitive from "react-resizable-panels"
+
+import { cn } from "@/lib/utils"
+
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}) => (
+  <ResizablePrimitive.PanelGroup
+    className={cn(
+      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      className
+    )}
+    {...props} />
+)
+
+const ResizablePanel = ResizablePrimitive.Panel
+
+const ResizableHandle = ({
+  withHandle,
